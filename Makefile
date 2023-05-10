6f652199b5a71ef8b6036f181070312a07fc5c3c
MKekkar <mkekkar@ucsd.edu>
2023-05-10 12:37:48 -0700

    add starter code

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # CSE30 Makefile
 # 
 # EXE - objects we want to build
-# OBJS - objects we want to compile (from c)
+# OBJS - ojects we want to compile (from c)
 # 
 # We provide a phony target clean to rm all the .o files
 # 
@@ -11,21 +11,25 @@
 # Run make DEBUG= WARN= to run without debug and warnings
 # Run make clean to clean objects
 
-EXE = tree_lookup
-OBJS = tree_lookup.o parse_opts.o hash.o wrapper.o
+EXE = tree_lookup 
+all: $(EXE)
+HEAD = node.h
+OBJS = tree_lookup.o node_lookup.o insert_node.o load_table.o  print_info.o \
+       delete_table.o hash.o
+ASM = hash.S
 CC = gcc
 DEBUG = -g
-WARN = -Wall -Wextra
-CFLAGS = -I. -std=gnu11 $(DEBUG) $(WARN)
-LDFLAGS = -Wl,--wrap=main,--wrap=malloc
+WARN = -Wall -Wextra -Werror -Wformat-security
+CFLAGS = -I. -std=gnu11 $(DEBUG) $(WARN) -O0
 LIBS = 
 
 .PHONY: clean
 
-$(EXE) : $(OBJS)
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $(OBJS) $(LIBS)
+%.o : %.c $(HEAD)
+	$(CC) $(CFLAGS) $< -c
 
-tree_lookup.o: tree_lookup.h
+$(EXE):	$(OBJS)
+	$(CC) $(CFLAGS) -o $@ $(OBJS) $(ASM) $(LIBS)
 
 clean:
 	rm -f $(EXE) $(OBJS)
--- hash.s
+++ hash.s
@@ -1,23 +1,20 @@
 // CES30 assignment template
 // 
+// cse30sp23id: 
 //
 // Describe target Hardware to the assembler
 .arch   armv6
-.arm
-.fpu	vfp
+.cpu    cortex-a53
 .syntax unified
-.align 4
-.bss
-.data
 /////////////////////////////////////////////////////////
 
 .text                       // start of the text segment
 /////////////////////////////////////////////////////////
 // function hashFun 
 /////////////////////////////////////////////////////////
-.type   hashFun, %function // define as a function
-.global hashFun            // export function name
-.equ    FP_OFFSET, 28      // (regs - 1) * 4
+.type   hashFun, %function  // define as a function
+.global hashFun             // export function name
+.equ    FP_OFFSET, 28       // (regs - 1) * 4
 /////////////////////////////////////////////////////////
 
 // put any .equ for hashFun here - delete this line
@@ -27,19 +24,16 @@
 hashFun:
     push    {r4-r9, fp, lr}     // use r4-r9 protected regs
     add     fp, sp, FP_OFFSET   // locate our frame pointer
-    // do not edit the prologue above
+    // DO NOT EDIT PROLOGUE ABOVE
     // You can use temporary r0-r3 and preserved r4-r9
     // Store return value (results) in r0
-    /////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
     
-    // TODO: Write your code here to implement the hash function.
-    // For reference, the C implementation is shown here:
-    // hash = c + (hash << 6) + (hash << 16) - hash;
-
-    /////////////////////////////////////////////////////////
-    // do not edit the epilogue below
+    // YOUR CODE GOES HERE
+    
+    ///////////////////////////////////////////////////////
+    // DO NOT EDIT EPILOGUE BELOW
     sub     sp, fp, FP_OFFSET   // restore sp
-    pop     {r4-r9,fp, lr}      // restore saved registers
-    bx      lr                  // function return 
-    .size   hashFun,(. - hashFun)
-
+    pop     {r4-r9, fp, lr}     // restore saved registers
+    bx      lr                  // function return
+    .size   hashFun,(.-hashFun)
